32804e8980ccb1872cf35df8d601b05db27445a8
Niles87 <nbing19@gmail.com>
2020-09-23 21:07:52 -0700

    added new packages and router to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import { Assignments } from "./components/Assignments";
+import { Attendance } from "./components/Attendance";
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
+import { Container } from "./components/Container";
+import { Nav } from "./components/Nav";
+import "./App.css";
 
 function App() {
+  const Default = () => (
+    <Fragment>
+      <Nav />
+      <Container>
+        <Route exact path="/assignments" component={Assignments} />
+        <Route exact path="/attendance" component={Attendance} />
+        <Route exact path="/home" component={Home} />
+      </Container>
+    </Fragment>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route component={Default} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

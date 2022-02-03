179a810bd5bc4edb42c70817458df6cb95a4779e
Niles Bingham <nbing19@gmail.com>
2022-02-03 15:55:17 -0800

    Added labels to all form elements throughout the site
    1. Changed divs to buttons on create page
    2. Changed div to footer on footer
    3. Added styles for readablity in modals

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -4,9 +4,11 @@ import "./footer.css";
 export const Footer = () => {
   const date = new Date().getFullYear();
   return (
-    <div className="footer">
+    <footer className="footer">
       <p>&copy; {date} Bit Academy</p>
-      <a href="https://github.com/niles87/bit-academy">github</a>
-    </div>
+      <a href="https://github.com/niles87/bit-academy">
+        github <span className="sr-only">, repository for application</span>
+      </a>
+    </footer>
   );
 };
